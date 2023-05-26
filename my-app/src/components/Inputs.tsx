639837fe7abb63f14a9f1389c85b8459ddbdbb33
Theojeddy <Theojeddy@hotmail.co.uk>
2023-05-26 14:15:42 +0100

    libraries installed

--- my-app/src/components/Inputs.tsx
+++ my-app/src/components/Inputs.tsx
@@ -1,7 +1,27 @@
+import React from 'react'; import { Button } from 'primereact/button';
+import { InputText } from "primereact/inputtext";
+
+
 function Inputs() {
 
   return (
-    <div className="inputs">Inputs</div>
+    <div className="inputs">Inputs
+    <div className="card flex justify-content-center">
+            <Button label="Submit" />
+        </div>
+        <br />
+        <div className="card flex flex-wrap justify-content-center gap-3">
+        <span className="p-input-icon-left">
+            <i className="pi pi-search" />
+            <InputText placeholder="Search" />
+        </span>
+
+        <span className="p-input-icon-right">
+            <i className="pi pi-spin pi-spinner" />
+            <InputText />
+        </span>
+    </div>
+    </div>
   )
 }
 
